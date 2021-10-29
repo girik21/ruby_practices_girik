46b8058200f5dcf1d70bcbe8dd12922908af7a0e
Kuniaki Igarashi <igaiga@gmail.com>
2021-10-29 18:32:10 +0900

    Feedback chapter 4

--- chapter4/drinks3.rb
+++ chapter4/drinks3.rb
@@ -1,5 +1,5 @@
 # Display first element "coffee" and the last element "mocha"
 drinks = ["coffee", "latte", "mocha"]
 puts drinks [0]
-puts drinks [1]
+puts drinks [1] #igiaga: This code return "latte".
 
