ecbb68eb3495b2f00eb9405d4485bab7f75f2435
Kuniaki Igarashi <igaiga@gmail.com>
2021-10-29 18:45:34 +0900

    Feedback chapter 6

--- chapter6/count.rb
+++ chapter6/count.rb
@@ -1,4 +1,5 @@
 #Char method in ruby
+# igaiga: Start with "caffelatte" and write the code to display {"c"=>1, "a"=>2, "f"=>2, "e"=>2, "l"=>1, "t"=>2}
 menu = {c: 1, a: 2, f: 2, e: 2, l: 1, t: 2}
 menu.each do |key, value| 
 puts "#{key}->#{value}"
--- chapter6/hash5.rb
+++ chapter6/hash5.rb
@@ -1,7 +1,7 @@
-#If condition in hash
+# If condition in hash
 menu = {coffee: 300, caffe_latte: 400}
-if menu[ :caffe_latte]  <=  500
-puts "Cafe Latte please"
+if menu[:caffe_latte]  <=  500
+  puts "Cafe Latte please"
 else
-puts"You can't get it!"
+  puts "You can't get it!"
 end
