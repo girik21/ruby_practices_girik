35e19024e6a713320ed16d3ebc29a21e021cf4c3
Kuniaki Igarashi <igaiga@gmail.com>
2021-11-10 18:25:11 +0900

    Feedback from igaiga

--- chapter10/1.rb
+++ chapter10/1.rb
@@ -1,5 +1,6 @@
 #Using sinatra
 require "sinatra"
 get "/luck" do
-    p ["Daikichi"  ,  "Nakakichi" ,  "Shukichi" , "Koshi"].sample
+  p ["Daikichi"  ,  "Nakakichi" ,  "Shukichi" , "Koshi"].sample
+  # the p method is not needed.
 end
